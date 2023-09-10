e78681ba73ced7ddf3b0b348316a02389d258992
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-09-10 17:04:16 -0500

    Add ScaleTransition

--- lib/src/pages/page1_page.dart
+++ lib/src/pages/page1_page.dart
@@ -28,11 +28,16 @@ class Page1Page extends StatelessWidget {
       // transitionDuration: Duration(seconds: 2),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
-        return SlideTransition(
-          // position: Tween<Offset>(begin: Offset(0.5,1.0), end: Offset.zero).animate(curvedAnimation),
-          // position: Tween<Offset>(begin: Offset(-0.5,1.0), end: Offset.zero).animate(curvedAnimation),
-          // position: Tween<Offset>(begin: Offset(0.0,1.0), end: Offset.zero).animate(curvedAnimation),
-          position: Tween<Offset>(begin: Offset(0.0,-1.0), end: Offset.zero).animate(curvedAnimation),
+        // return SlideTransition(
+        //   // position: Tween<Offset>(begin: Offset(0.5,1.0), end: Offset.zero).animate(curvedAnimation),
+        //   // position: Tween<Offset>(begin: Offset(-0.5,1.0), end: Offset.zero).animate(curvedAnimation),
+        //   // position: Tween<Offset>(begin: Offset(0.0,1.0), end: Offset.zero).animate(curvedAnimation),
+        //   position: Tween<Offset>(begin: Offset(0.0,-1.0), end: Offset.zero).animate(curvedAnimation),
+        //   child: child,
+        // );
+
+        return ScaleTransition(
+          scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
           child: child,
         );
       },
