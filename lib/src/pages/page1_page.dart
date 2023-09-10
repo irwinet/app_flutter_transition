d80da41236496be35245b365685d7d7d15209025
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-09-10 17:02:31 -0500

    Add SlideTransition

--- lib/src/pages/page1_page.dart
+++ lib/src/pages/page1_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_flutter_transition/src/pages/page2_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,9 +16,26 @@ class Page1Page extends StatelessWidget {
      floatingActionButton: FloatingActionButton(
       child: Icon(Icons.access_time),
       onPressed: (){
-
+        Navigator.push(context, _crearRuta());
       },
      ),
    );
+  }
+  
+  Route _crearRuta() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => Page2Page(),
+      // transitionDuration: Duration(seconds: 2),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+        return SlideTransition(
+          // position: Tween<Offset>(begin: Offset(0.5,1.0), end: Offset.zero).animate(curvedAnimation),
+          // position: Tween<Offset>(begin: Offset(-0.5,1.0), end: Offset.zero).animate(curvedAnimation),
+          // position: Tween<Offset>(begin: Offset(0.0,1.0), end: Offset.zero).animate(curvedAnimation),
+          position: Tween<Offset>(begin: Offset(0.0,-1.0), end: Offset.zero).animate(curvedAnimation),
+          child: child,
+        );
+      },
+    );
   }
 }
